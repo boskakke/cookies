05fb406b05a28fa984a1915af2d44ee52bcbece6
Bo Skakke <bsk@berlingskemedia.dk>
2018-05-28 22:45:51 +0200

    first

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<link rel="stylesheet" href="/dist/styles/main.css">
 </head>
 
-
+<body>
 
 <div class="qc-cmp-ui-container qc-cmp-showing"><div class="qc-cmp-ui qc-cmp-showing" id="qcCmpUi"><img class="qc-cmp-publisher-logo qc-cmp-hidden" src="" alt="JP/Politkens hus logo" onerror="window.__cmpui(&quot;hideLogo&quot;,event)">      <div class="qc-cmp-ui-content">      <h1 class="qc-cmp-title"> Privatlivsvilkår </h1>      <span>      <p class="qc-cmp-main-messaging">
 
@@ -20,47 +20,13 @@
 
 
    <span>      <div class="qc-cmp-buttons" id="qcCmpButtons">      <button class="qc-cmp-button" onclick="window.__cmpui(&quot;setAndSaveAllConsent&quot;,!0)"> Acceptér </button>      </div>      </span>      </span>      </div>      <div class="qc-cmp-initalt-buttons">      <p><a id="qc-cmp-purpose-button" class="qc-cmp-alt-action qc-cmp-center-bottom" onclick="window.__cmpui(&quot;updateConsentUi&quot;,2)"> Administrér samtykke </a></p>      <!-- <p class="qc-smaller">Læs i øvrigt vores opdaterede <a href="http://www.jppol.dk/da/specialsider/cookies.aspx" target="_blank">persondata politik her</a></p>       --></div></div></div>
-	
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="display: none;">
-	<defs>
-
-
-		<symbol id="logo">
-			<title>Settings</title>
-			<path class="icon" d="M990,572.1c0,9.6-7.7,21.1-17.9,23l-118,17.9c-7,20.4-14.7,39.6-24.9,58.1c21.7,31.3,44.7,59.3,68.3,88c3.8,4.5,6.4,10.2,6.4,16s-1.9,10.2-5.7,14.7C882.8,810.1,796.7,903.9,775,903.9c-5.7,0-11.5-2.6-16.6-5.7l-88-68.9c-18.5,9.6-38.3,17.9-58.1,24.2c-4.5,38.9-8.3,80.4-18.5,118.7c-2.6,10.2-11.5,17.9-23,17.9H429.2c-11.5,0-21.7-8.3-23-19.1l-17.9-117.4c-19.8-6.4-38.9-14-57.4-23.6l-90,68.3c-4.5,3.8-10.2,5.7-16,5.7c-5.7,0-11.5-2.6-16-7c-33.8-30.6-78.5-70.2-105.3-107.2c-3.2-4.5-4.5-9.6-4.5-14.7c0-5.7,1.9-10.2,5.1-14.7c21.7-29.3,45.3-57.4,67-87.4c-10.8-20.4-19.8-41.5-26.2-63.2L28.5,592.5c-10.8-1.9-18.5-12.1-18.5-23V427.9c0-9.6,7.7-21.1,17.2-23l118.7-17.9c6.4-20.4,14.7-39.6,24.9-58.7c-21.7-30.6-44.7-59.3-68.3-88c-3.8-4.5-6.4-9.6-6.4-15.3c0-5.7,2.6-10.2,5.7-14.7C117.2,189.3,203.3,96.1,225,96.1c5.7,0,11.5,2.6,16.6,6.4l88,68.3c18.5-9.6,38.3-17.9,58.1-24.2c4.5-38.9,8.3-80.4,18.5-118.7c2.6-10.2,11.5-17.9,23-17.9h141.6c11.5,0,21.7,8.3,23,19.1l17.9,117.4c19.8,6.4,38.9,14,57.4,23.6l90.6-68.3c3.8-3.8,9.6-5.7,15.3-5.7c5.7,0,11.5,2.6,16,6.4c33.8,31.3,78.5,70.8,105.3,108.5c3.2,3.8,4.5,8.9,4.5,14c0,5.7-1.9,10.2-5.1,14.7c-21.7,29.3-45.3,57.4-67,87.4c10.8,20.4,19.8,41.5,26.2,62.5l116.8,17.9c10.8,1.9,18.5,12.1,18.5,23L990,572.1L990,572.1z M500,336.7c-90,0-163.3,73.4-163.3,163.3c0,90,73.4,163.3,163.3,163.3c90,0,163.3-73.4,163.3-163.3S590,336.7,500,336.7z"/>
-
-		</symbol>
-	</defs>
-</svg>
 
 
 
-<body>
+
+
 
 	
-	<div class="content ">
-		<div class="cookie__content">
-			<div class="header">
-				Privatlivsvilkår
-			</div>
-			<p>
-			Vi anvender blandet andet cookies for at kunne målrette annoncering. Ved at klikke "OK" giver du samtykke til vores samarbejdspartneres anvendelse af cookies på dette website. Du kan altid trække dit samtykke tilbage via linket "administrér samtykke". </p>
-
-			<p>Få ydligere oplysninger ved at læse om vores samarbejdspartneres <a href="#">behandling af persondata</a> eller vores <a href="#">politik for behandling af persondata</a>.</p>
-
-			
-			<div class="cookie-footer">
-				<button class="btn btn--lg btn--block btn--blue">OK</button>
-
-				<button class="btn btn--sm btn--trans">
-					<svg viewBox="0 0 1000 1000" class="icon">
-						<use xlink:href="#logo"></use>
-					</svg> Administrér samtykke</button>	
-			</div>
-
-		</div>
-	</div>
-
 
 
 
